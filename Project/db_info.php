8e437633d6a1cfd6bd3ac5d235f71cbe4e16393a
Leon Shi <leonshiyan@hotmail.com>
2017-11-05 09:34:23 -0800

    Personal info stored in this page is erased

--- Project/db_info.php
+++ Project/db_info.php
@@ -1,6 +1,6 @@
 <?php
 	$host = "localhost";
-	$user = "saintpat_2261";
-	$password = "Michael";
-	$dbname = "saintpat_database";
+	$user = "yourname";
+	$password = "yourpsw";
+	$dbname = "yourdb";
 ?>
